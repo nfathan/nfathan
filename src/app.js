f09fa83e19b8be7028d8ced4a7fe473bda11bb7a
nfathan <athan.354@gmail.com>
2021-09-21 00:02:08 +0800

    add import footer

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import './public/images/nfathan.jpg';
+import './public/images/dicoding-quote.jpg';
+import './public/images/css.png';
+import './public/images/html.png';
+import './public/images/js.png';
+import './public/images/bootstrap.png';
+import './public/images/node.png';
+import './public/images/react.png';
+import './public/images/webpack.png';
+import './public/images/sass.png';
+import './public/images/kuy-masak.png';
+import './public/images/kalkulator.png';
+import './public/images/pia-cianni.png';
+import './public/images/tic-tac-toe.png';
+
+import './styles/style.css';
+import './styles/responsive.css';
+import header from './script/view/header.js';
+import main from './script/view/main.js';
+import footer from './script/view/footer'
+import './script/data/storage.js';
+
+document.addEventListener("DOMContentLoaded", header);
+document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", footer);
+
