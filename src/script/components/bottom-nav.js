b2738236202e243499b2c02522bdffe90e7ae0d3
nfathan <athan.354@gmail.com>
2021-09-21 18:48:59 +0800

    update ui, add bottom nav

--- src/script/components/bottom-nav.js
+++ src/script/components/bottom-nav.js
@@ -31,112 +31,96 @@ class BottomNav extends HTMLElement {
           width: 100%;
           text-align: center;
           margin: 10px 0;
-          padding; 20px;
         }
       }
       
       /* smartphone ==================== */
       @media (max-width: 670px) {
-          bottom-nav {
-            display: block;
-            position: fixed;
-            bottom: 0;
-            z-index: 1;
-            width: 100%;
-            border-top: 1px solid rgba(255, 255, 255, 0.1);
-            background-color: rgba(17, 25, 40, 0.99);
-          }
-
-          .bottom-nav-list {
-            display: flex;
-            color : #9ca3af;
-          }
-
-          .bottom-nav-item {
-            width: 100%;
-            text-align: center;
-            margin: 10px 0;
-            padding; 20px;
-          }
+        .bottom-nav-item {
+          margin: 5px 0;
+        }
       }
 
       /* sidebar ================================ */
         .sidebar-icon {
-            display: none;
-            font-size: 26px;
-            background-color: transparent;
-            color: #6c7686;
-            margin-top: 20px;
-            padding: 0 15px;
-            border: 1px solid rgba(255, 255, 255, 0.125);
-            border-radius: 10px
+          display: none;
+          font-size: 26px;
+          background-color: transparent;
+          color: #6c7686;
+          margin-top: 20px;
+          padding: 0 15px;
+          border: 1px solid rgba(255, 255, 255, 0.125);
+          border-radius: 10px
         }
         
         .sidebar {
-            display: none;
-            z-index: 1;
-            width: 300px;
-            height: calc(100% - 60px);
-            top: 0;
-            left: 0;
-            position: fixed;
-            font-size: 14px;
-            font-weight: 600;
-            border-right: 1px solid rgba(255, 255, 255, 0.125);
-            box-shadow: 0px 10px 30px 0px rgba(0, 0, 0, 0.1);
-            transform: translate(-310px, 0);
-            transition: transform 0.3s ease-in-out;
-            backdrop-filter: blur(16px) saturate(180%);
-            -webkit-backdrop-filter: blur(16px) saturate(180%);
-            background-color: rgba(255, 255, 255, 0.1);
-            
+          display: none;
+          z-index: 1;
+          width: 300px;
+          height: calc(100% - 60px);
+          top: 0;
+          left: 0;
+          position: fixed;
+          font-size: 14px;
+          font-weight: 600;
+          border-right: 1px solid rgba(255, 255, 255, 0.125);
+          box-shadow: 0px 10px 30px 0px rgba(0, 0, 0, 0.1);
+          transform: translate(-310px, 0);
+          transition: transform 0.3s ease-in-out;
+          backdrop-filter: blur(16px) saturate(180%);
+          -webkit-backdrop-filter: blur(16px) saturate(180%);
+          background-color: rgba(255, 255, 255, 0.1);
         }
         
         .sidebar.open {
-            transform: translate(0,0)
+          transform: translate(0,0)
         }
         
         .sidebar-menu {
-            padding: 20px 20px;
+          padding: 20px;
         }
         
         .sidebar-list {
-            padding: 10px 0px;
+          text-align: left;
+          margin-top: 40px;
+          padding: 10px 0;
+          border-radius: 10px;
+          border-top: 1px solid rgba(255, 255, 255, 0.125);
         }
         
         .sidebar-list li {
-            padding: 10px 10px;
-            list-style-type: none;
+          padding: 10px 0;
+          list-style-type: none;
         }
         
         .sidebar-item {
-            padding: 10px 10px;
+          padding: 10px;
         }
         
         .sidebar-item:hover {
-            border-bottom: 2px solid #6c7686;
+          border-bottom: 2px solid #6c7686;
         }
         
         .sidebar-item:focus {
-            border-bottom: 2px solid #a1a6af;
+          border-bottom: 2px solid #a1a6af;
         }
         
         /* tablet ==================== */
         @media (max-width: 1000px) {        
-            .sidebar-icon {
-                display: block;
-            }
-        
-            .sidebar {
-                display: block;
-            }
+          .sidebar-icon {
+            display: block;
+          }
+      
+          .sidebar {
+            display: block;
+          }
         }
         
         /* smartphone ==================== */
         @media (max-width: 670px) {
-            .sidebar-icon {
-                display: block;
-            } 
+          .sidebar-icon {
+            display: block;
+          } 
         }
       </style>
 
@@ -145,12 +129,23 @@ class BottomNav extends HTMLElement {
           <button class="menu icon" type="button"> &#9776 </button>
         </div>
         <div class="bottom-nav-item">
-          <button class="home icon" type="button"> &#8962 </button>
+          <a class="home icon" href="/"> &#8962 </a>
         </div>
         <div class="bottom-nav-item">
-          <button class="contact icon" type="button"> &#128172 </button>
+          <button class="contact icon" type="button" onclick="openTab('contact', this, '#f6faff')"> &#128172 </button>
         </div>
       </div>
+
+      <div class="sidebar">
+        <div class="sidebar-menu">
+          <button class="closebtn" type="button">&#120;</button>
+          <ul class="sidebar-list">
+            <li><a class="sidebar-item" onclick="openTab('experience', this, '#58d2b2')" href="#experience">Experience</a></li>
+            <li><a class="sidebar-item" onclick="openTab('education', this, '#58d2b2')" href="#education">Education</a></li>
+            <li><a class="sidebar-item" onclick="openTab('PiaCianni', this, '#58d2b2')" href="#PiaCianni">Pia Cianni</a></li>
+          </ul>
+        </div>
+        </div>
       `;
   }
 }
