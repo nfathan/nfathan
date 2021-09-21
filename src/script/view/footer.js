1e4232a25b5aa148de4a7f921b424b925de42c62
nfathan <athan.354@gmail.com>
2021-09-21 18:56:16 +0800

    add bottom nav

--- src/script/view/footer.js
+++ src/script/view/footer.js
@@ -1,9 +1,10 @@
 import '../components/bottom-nav'
 
 const footer = () => {
+  // bottom navigation
   const menuIcon = document.querySelector(".menu")
   const sidebar = document.querySelector(".sidebar");
-  const closeSidebar = document.querySelector(".content-wrapper");
+  const closeSidebar = document.querySelector(".closebtn");
 
   menuIcon.addEventListener("click", event => { //mengklik sidebar-icon
     sidebar.classList.toggle("open"); //membuka sidebar-content 
